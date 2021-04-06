106f15ef9c32cee457d65d1a8c85d663664c90ff
Pedro Arturo Higuera Garzón <higueragarzonpedroarturo@gmail.com>
2021-04-06 14:31:44 -0500

    Se desarrolló la ventana principal de la app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:security_app/startPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Security App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StartPage(),
+    );
+  }
+}
